d87ded46a4879e728c9f7c5cd63ee381bab9df04
zschira <zach.schira@catalyst.coop>
2022-08-18 14:37:02 -0400

    Test locale fix

--- arelle/Locale.py
+++ arelle/Locale.py
@@ -61,7 +61,7 @@ def getUserLocale(localeCode=''):
                         break
                     except locale.Error:
                         pass # this one didn't work
-    locale.setlocale(locale.LC_ALL, _STR_8BIT('C'))  # str needed for 3to2 2.7 python to work
+    # locale.setlocale(locale.LC_ALL, _STR_8BIT('C'))  # str needed for 3to2 2.7 python to work
     if conv is None: # some other issue prevents getting culture code, use 'C' defaults (no thousands sep, no currency, etc)
         conv = locale.localeconv() # use 'C' environment, e.g., en_US
     if C_LOCALE is None: # load culture-invariant C locale
